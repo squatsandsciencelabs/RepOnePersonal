29a1b71f5513e116bdc8d7d5373327a1ae277877
JonnyLinja <jonny.linja@gmail.com>
2020-01-29 18:42:25 -0500

    now hides install button if not connected

--- app/features/settings/ota/panels/SettingsOTAReadyPanel.js
+++ app/features/settings/ota/panels/SettingsOTAReadyPanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Fragment } from 'react-native';
 import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
 
 class SettingsOTAReadyPanel extends Component {
@@ -7,6 +7,20 @@ class SettingsOTAReadyPanel extends Component {
     // TODO: bold the version numbers
     render() {
         const deviceFirmwareText = this.props.deviceFirmwareVersion ? `The connected RepOne unit is version ${this.props.deviceFirmwareVersion}` : 'Connect a device to compare versions';
+        const install = this.props.connectedDevice ? (<Fragment>
+            <TouchableOpacity style={[SETTINGS_PANEL_STYLES.blueButton, {height: 50}]}
+                onPress={this.props.install.bind(this)}>
+                    <Text style={SETTINGS_PANEL_STYLES.buttonText}>Install on RepOne #{this.props.connectedDevice}</Text>
+            </TouchableOpacity>
+            <Text style={ SETTINGS_PANEL_STYLES.footerCancelText }>
+                To install on another RepOne unit, connect to it below.
+            </Text>
+        </Fragment>) : (
+            <Text style={ SETTINGS_PANEL_STYLES.footerCancelText }>
+                To install on a RepOne unit, connect to it below.
+            </Text>
+        );
+
         return (
             <View style={ [SETTINGS_PANEL_STYLES.panel, { flex: 1 }] }>
                 <View style={ SETTINGS_PANEL_STYLES.header }>
@@ -26,13 +40,7 @@ class SettingsOTAReadyPanel extends Component {
                     <Text style={ SETTINGS_PANEL_STYLES.subtitleText }>
                         Version {this.props.firmwareVersion} files downloaded
                     </Text>
-                    <TouchableOpacity style={[SETTINGS_PANEL_STYLES.blueButton, {height: 50}]}
-                        onPress={this.props.install.bind(this)}>
-                            <Text style={SETTINGS_PANEL_STYLES.buttonText}>Install on RepOne #{this.props.connectedDevice}</Text>
-                    </TouchableOpacity>
-                    <Text style={ SETTINGS_PANEL_STYLES.footerCancelText }>
-                        To install on another RepOne unit, connect to it below.
-                    </Text>
+                    {install}
                     <Text style={SETTINGS_PANEL_STYLES.tappableText} onPress={this.props.deleteDownload.bind(this)}>
                         I'm done, delete update files
                     </Text>
