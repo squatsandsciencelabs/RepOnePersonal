bfb1bd7acb492d0f9119ec6eb7361b47c4ed5670
JonnyLinja <jonny.linja@gmail.com>
2020-03-07 02:47:33 -0500

    bulk data selectors

---
+++ app/redux/selectors/BulkDataSelectors.js
@@ -0,0 +1,24 @@
+const stateRoot = (state) => state.bulkData;
+
+export const getTotalSampleCount = (state, deviceRepID) => {
+    return stateRoot(state)[deviceRepID] ? stateRoot(state)[deviceRepID].totalSampleCount : null;
+};
+
+export const areAllSamplesReceived = (state, deviceRepID) => {
+    if (!stateRoot(state)[deviceRepID] || stateRoot(state)[deviceRepID].totalSampleCount === null || stateRoot(state)[deviceRepID].totalSampleCount < stateRoot(state)[deviceRepID].receivedSampleCount) {
+        return false;
+    }
+    return true;
+};
+
+export const getRepBulkData = (state, deviceRepID) => {
+    return stateRoot(state)[deviceRepID].bulk;
+};
+
+export const getRepIndex = (state, deviceRepID) => {
+    return stateRoot(state)[deviceRepID].repIndex;
+}; 
+
+export const getSetID = (state, deviceRepID) => {
+    return stateRoot(state)[deviceRepID].setID;
+};
