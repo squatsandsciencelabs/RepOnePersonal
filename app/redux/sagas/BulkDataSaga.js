f23cbd49964d939a0e20391201b45819dcda8c63
JonnyLinja <jonny.linja@gmail.com>
2020-03-08 23:58:36 -0400

    removed unneeded yield spawns. grab sample count now happens on add rep and keeps trying. write now should have max byte size and array

--- app/redux/sagas/BulkDataSaga.js
+++ app/redux/sagas/BulkDataSaga.js
@@ -9,6 +9,8 @@ import {
     CLEAR_BULK_DATA_MAP,
     SAVE_WORKOUT_REP,
     SAVE_HISTORY_REP,
+
+    CONNECTED_TO_DEVICE,
 } from 'app/configs+constants/ActionTypes';
 
 import * as ConnectedDeviceStatusSelectors from 'app/redux/selectors/ConnectedDeviceStatusSelectors';
@@ -19,7 +21,6 @@ export default function *BulkDataSaga() {
     yield all([
         takeEvery(ADD_REP_DATA, updateBulkReducer),
         takeEvery(ADD_BULK_DATA, addBulkData),
-        takeEvery(UPDATE_BULK_SAMPLE_COUNT, updateBulkSampleCount),
     ]);
 };
 
@@ -28,94 +29,94 @@ function *updateBulkReducer(action) {
         return;
     }
     
+    // get vars
     const workoutData = yield select(SetsSelectors.getWorkoutSets);
     const set = workoutData[workoutData.length-1];
     const setID = set.setID;
     const repIndex = sets.reps ? set.reps.length-1 : 0;
 
+    // map it
     yield put({
         type: MAP_BULK_DATA,
         deviceRepID: action.deviceRepID,
         setID,
         repIndex,
     });
+
+    // bluetooth attempt
+    yield call(updateBulkSampleCount);
 }
 
 function *addBulkData(action) {
     const state = yield select();
 
-    // request sample count
-    const totalSampleCount = BulkDataSelectors.getTotalSampleCount(state, action.deviceRepID);
-    if (totalSampleCount === null) {
-        yield spawn(updateBulkSampleCount);
+    // completed check
+    const completed = BulkDataSelectors.areAllSamplesReceived(state, action.deviceRepID);
+    if (!completed) {
         return;
     }
 
-    // completion
-    const completed = BulkDataSelectors.areAllSamplesReceived(state, action.deviceRepID);
-    if (completed) {
-        // TODO: inform saving of the thing
-        const repIndex = BulkDataSelectors.getRepIndex(state, action.deviceRepID);
-        const setID = BulkDataSelectors.getSetID(state, action.deviceRepID);
-        const bulkData = BulkDataSelectors.getRepBulkData(state, action.deviceRepID);
-        if (SetsSelectors.getHistorySet(state, setID)) {
-            // history has it
-            yield put({
-                type: SAVE_HISTORY_REP,
-                setID,
-                repIndex,
-                bulkData,
-            });
-        } else if (SetsSelectors.getWorkoutSet(state, setID)) {
-            // workout has it
-            yield put({
-                type: SAVE_WORKOUT_REP,
-                setID,
-                repIndex,
-                bulkData,
-            });
-        }
-
-        // clear it locally
+    // save to datastore
+    const repIndex = BulkDataSelectors.getRepIndex(state, action.deviceRepID);
+    const setID = BulkDataSelectors.getSetID(state, action.deviceRepID);
+    const bulkData = BulkDataSelectors.getRepBulkData(state, action.deviceRepID);
+    if (SetsSelectors.getHistorySet(state, setID)) {
+        // history has it
         yield put({
-            type: CLEAR_BULK_DATA_MAP,
-            deviceRepID: action.deviceRepID,
+            type: SAVE_HISTORY_REP,
+            setID,
+            repIndex,
+            bulkData,
         });
-
-        // tell the sensor it's okay
-        yield spawn(writeToSensor)
+    } else if (SetsSelectors.getWorkoutSet(state, setID)) {
+        // workout has it
+        yield put({
+            type: SAVE_WORKOUT_REP,
+            setID,
+            repIndex,
+            bulkData,
+        });
     }
-}
 
-function *writeToSensor() {
+    // clear it locally
+    yield put({
+        type: CLEAR_BULK_DATA_MAP,
+        deviceRepID: action.deviceRepID,
+    });
+
+    // tell the sensor it's okay
     const deviceIdentifier = yield select(ConnectedDeviceStatusSelectors.getConnectedDeviceIdentifier);
     if (!deviceIdentifier) {
         console.tron.log(`Unable to add bulk sample count as no device connected`);
         return;
     }
-
-    yield apply(BleManager, BleManager.write, deviceIdentifier, 'A5183278-CA65-45B7-B6C3-A68552F2026D', 'A5183278-CA65-45B7-B6C3-A68552F20274');
+    yield apply(BleManager, BleManager.write, deviceIdentifier, 'A5183278-CA65-45B7-B6C3-A68552F2026D', 'A5183278-CA65-45B7-B6C3-A68552F20274', [action.deviceRepID], 2);
 }
 
-function *updateBulkSampleCount(action) {
-    const deviceIdentifier = yield select(ConnectedDeviceStatusSelectors.getConnectedDeviceIdentifier);
-    if (!deviceIdentifier) {
-        console.tron.log(`Unable to update bulk sample count as no device connected`);
-        return;
-    }
+// TODO: consider what happens if sensors are disconnected, switched, and so on
+function *updateBulkSampleCount() {
+    while (true) {
+        const deviceIdentifier = yield select(ConnectedDeviceStatusSelectors.getConnectedDeviceIdentifier);
+        if (!deviceIdentifier) {
+            console.tron.log(`Unable to update bulk sample count as no device connected, trying again once connected to a device`);
+            yield take(CONNECTED_TO_DEVICE);
+            continue;
+        }
 
-    try {
-        const response = yield apply(BleManager, BleManager.read, deviceIdentifier, 'A5183278-CA65-45B7-B6C3-A68552F2026D', 'A5183278-CA65-45B7-B6C3-A68552F20274');
-        const typedArray = new Uint8Array(response);
-        const data16 = new Uint16Array(typedArray.buffer);
-        const deviceRepID = data16[0];
-        const totalSampleCount = data16[1];
-        yield put({
-            type: UPDATE_BULK_SAMPLE_COUNT,
-            deviceRepID,
-            totalSampleCount,
-        });
-    } catch (err) {
-        console.tron.log(`Error updating bulk sample count`);
+        try {
+            const response = yield apply(BleManager, BleManager.read, deviceIdentifier, 'A5183278-CA65-45B7-B6C3-A68552F2026D', 'A5183278-CA65-45B7-B6C3-A68552F20274');
+            const typedArray = new Uint8Array(response);
+            const data16 = new Uint16Array(typedArray.buffer);
+            const deviceRepID = data16[0];
+            const totalSampleCount = data16[1];
+            yield put({
+                type: UPDATE_BULK_SAMPLE_COUNT,
+                deviceRepID,
+                totalSampleCount,
+            });
+            return;
+        } catch (err) {
+            console.tron.log(`Error updating bulk sample count ${err}, trying again immediately`);
+        }
     }
 }
