c8ade421a4e90004fe896ab3322cdef70201f943
JonnyLinja <jonny.linja@gmail.com>
2020-04-01 00:53:24 -0400

    removed debugging logs

--- app/math/VersionComparison.js
+++ app/math/VersionComparison.js
@@ -10,29 +10,22 @@ export const isVersionLessThanOrEqual = (version, compare) => {
     version = versionArrayFromString(version);
     compare = versionArrayFromString(compare);
     if (version[0] > compare[0]) {
-        console.tron.log(`major ${version[0]} > ${compare[0]}`);
         return false;
     } else if (version[0] < compare[0]) {
-        console.tron.log(`major ${version[0]} < ${compare[0]}`);
         return true;
     }
     if (version[1] > compare[1]) {
-        console.tron.log(`minor ${version[1]} > ${compare[1]}`);
         return false;
     } else if (version[1] < compare[1]) {
-        console.tron.log(`minor ${version[1]} < ${compare[1]}`);
         return true;
     }
     if (version[2] > compare[2]) {
-        console.tron.log(`patch ${version[2]} > ${compare[2]}`);
         return false;
     } else if (version[2] < compare[2]) {
-        console.tron.log(`patch ${version[2]} < ${compare[2]}`);
         return true;
     }
 
     // full equal
-    console.tron.log(`${version} == ${compare}`);
     return true;
 };
 
@@ -40,29 +33,22 @@ export const isVersionGreaterThanOrEqual = (version, compare) => {
     version = versionArrayFromString(version);
     compare = versionArrayFromString(compare);
     if (version[0] < compare[0]) {
-        console.tron.log(`major ${version[0]} < ${compare[0]}`);
         return false;
     } else if (version[0] > compare[0]) {
-        console.tron.log(`major ${version[0]} > ${compare[0]}`);
         return true;
     }
     if (version[1] < compare[1]) {
-        console.tron.log(`minor ${version[1]} < ${compare[1]}`);
         return false;
     } else if (version[1] > compare[1]) {
-        console.tron.log(`minor ${version[1]} > ${compare[1]}`);
         return true;
     }
     if (version[2] < compare[2]) {
-        console.tron.log(`patch ${version[2]} < ${compare[2]}`);
         return false;
     } else if (version[2] > compare[2]) {
-        console.tron.log(`patch ${version[2]} > ${compare[2]}`);
         return true;
     }
 
     // full equal
-    console.tron.log(`${version} == ${compare}`);
     return true;
 };
 
@@ -70,28 +56,21 @@ export const isVersionGreaterThan = (version, compare) => {
     version = versionArrayFromString(version);
     compare = versionArrayFromString(compare);
     if (version[0] < compare[0]) {
-        console.tron.log(`major ${version[0]} < ${compare[0]}`);
         return false;
     } else if (version[0] > compare[0]) {
-        console.tron.log(`major ${version[0]} > ${compare[0]}`);
         return true;
     }
     if (version[1] < compare[1]) {
-        console.tron.log(`minor ${version[1]} < ${compare[1]}`);
         return false;
     } else if (version[1] > compare[1]) {
-        console.tron.log(`minor ${version[1]} > ${compare[1]}`);
         return true;
     }
     if (version[2] < compare[2]) {
-        console.tron.log(`patch ${version[2]} < ${compare[2]}`);
         return false;
     } else if (version[2] > compare[2]) {
-        console.tron.log(`patch ${version[2]} > ${compare[2]}`);
         return true;
     }
 
     // full equal
-    console.tron.log(`${version} == ${compare}`);
     return false;
 };
