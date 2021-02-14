b3ede81cbc0ea813900dce8d3c4d05e9accfea18
JonnyLinja <jonny.linja@gmail.com>
2021-02-14 18:56:38 -0500

    update ip address of reactotron

--- app/configs+constants/ReactotronConfig.js
+++ app/configs+constants/ReactotronConfig.js
@@ -7,7 +7,7 @@ if (__DEV__) {
     Reactotron
     .configure({
         name: 'RepOne',
-        host: '192.168.0.10',
+        host: '192.168.1.102',
     }) // controls connection & communication settings
     .use(sagaPlugin()) // add all built-in react native plugins
     .use(reactotronRedux()) // add all built-in react native plugins
