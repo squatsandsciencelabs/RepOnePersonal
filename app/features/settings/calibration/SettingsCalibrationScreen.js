b908031112db3da0ed7c06925ea12610b9a93863
JonnyLinja <jonny.linja@gmail.com>
2021-02-22 14:20:35 -0500

    memoized calibration screen

--- app/features/settings/calibration/SettingsCalibrationScreen.js
+++ app/features/settings/calibration/SettingsCalibrationScreen.js
@@ -1,16 +1,20 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import SettingsCalibrationPanel from './SettingsCalibrationPanel';
 import * as Actions from './SettingsCalibrationActions';
 import * as ConnectedDeviceStatusSelectors from 'app/redux/selectors/ConnectedDeviceStatusSelectors';
 
-const mapStateToProps = (state) => {
-    const format = ConnectedDeviceStatusSelectors.getAPIFormatVersion(state);
-    return {
-        isVisible: format !== null && format >= 2,
-    };
-};
+// debatable if this should be memoized, but might be worth it given it's called every time and RARELY changes
+const selectMapStateToProps = createSelector(
+    ConnectedDeviceStatusSelectors.getAPIFormatVersion,
+    format => {
+        return {
+            isVisible: format !== null && format >= 2,
+        };
+    }
+)
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
@@ -20,7 +24,7 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 const SettingsCalibrationScreen = connect(
-    mapStateToProps,
+    selectMapStateToProps,
     mapDispatchToProps
 )(SettingsCalibrationPanel);
 
