9365046f6f57d1a69bc4dfbde7aca6aa09119cf9
JonnyLinja <jonny.linja@gmail.com>
2021-02-22 22:46:54 -0500

    memoized onermresultsscreen

--- app/features/analysis/results/OneRMResultsScreen.js
+++ app/features/analysis/results/OneRMResultsScreen.js
@@ -1,59 +1,101 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import OneRMResultsView from './OneRMResultsView';
 import * as Actions from './OneRMResultsActions';
-import * as SetsSelectors from 'app/redux/selectors/SetsSelectors';
 import * as SettingsSelectors from 'app/redux/selectors/SettingsSelectors';
 import * as AnalysisSelectors from 'app/redux/selectors/AnalysisSelectors';
-import * as AppStateActionCreators from 'app/redux/shared_actions/AppStateActionCreators';
-import * as OneRMCalculator from 'app/math/OneRMCalculator';
+
+const getShouldDisplayRegression = createSelector(
+    AnalysisSelectors.getIsR2HighEnough,
+    AnalysisSelectors.getRegressionPoints,
+    AnalysisSelectors.getActiveChartData,
+    AnalysisSelectors.getIsRegressionNegative,
+    (isR2HighEnough, regressionPoints, activeChartData, isNegative) => {
+        return isR2HighEnough
+        && regressionPoints
+        && regressionPoints.length === 2
+        && activeChartData.length >= 5
+        && isNegative
+    }
+);
+
+const getRegLeftPoint = createSelector(
+    AnalysisSelectors.getRegressionPoints,
+    getShouldDisplayRegression,
+    (points, shouldDisplayRegression) => {
+        if (!shouldDisplayRegression) {
+            return null;
+        }
+        return points[0];
+    }
+);
+
+const getRegRightPoint = createSelector(
+    AnalysisSelectors.getRegressionPoints,
+    getShouldDisplayRegression,
+    (points, shouldDisplayRegression) => {
+        if (!shouldDisplayRegression) {
+            return null;
+        }
+        return points[1];
+    }
+);
+
+const getE1RM = createSelector(
+    AnalysisSelectors.getE1RM,
+    getShouldDisplayRegression,
+    (e1RM, shouldDisplayRegression) => {
+        if (!shouldDisplayRegression) {
+            return null;
+        }
+        return e1RM;
+    }
+);
+
+const getMinX = createSelector(
+    AnalysisSelectors.getMinX,
+    x => {
+        return x * 0.9;
+    }
+);
+
+const getMaxX = createSelector(
+    AnalysisSelectors.getMaxX,
+    getRegRightPoint,
+    getShouldDisplayRegression,
+    (x, regRightPoint, shouldDisplayRegression) => {
+        const v = x * 1.1;
+        if (!shouldDisplayRegression) {
+            return v;
+        }
+        return Math.max(v, regRightPoint.x)
+    }
+);
+
+const getMaxY = createSelector(
+    AnalysisSelectors.getMaxY,
+    y => {
+        return y * 1.1;
+    }
+);
 
 const mapStateToProps = (state) => {
-    const activeChartData = AnalysisSelectors.getActiveChartData(state);
-    const errorChartData = AnalysisSelectors.getErrorChartData(state);
-    const unusedChartData = AnalysisSelectors.getUnusedChartData(state);
-    const regressionPoints = AnalysisSelectors.getRegressionPoints(state);
-    const isR2HighEnough = AnalysisSelectors.getIsR2HighEnough(state);
-    const minX = AnalysisSelectors.getMinX(state) * 0.9;
-    var maxX = AnalysisSelectors.getMaxX(state) * 1.1;
-    const maxY = AnalysisSelectors.getMaxY(state) * 1.1;
-
-    const shouldDisplayRegression = isR2HighEnough
-                                    && regressionPoints
-                                    && regressionPoints.length === 2
-                                    && activeChartData.length >= 5
-                                    && AnalysisSelectors.getIsRegressionNegative(state);
-
-    // TODO: store regression point and weight values instead of recalculating them every single time for speed
-    // otherwise it's slow on launch every time, especially with every action
-    if (shouldDisplayRegression) {
-        // there's a regression line
-        var regLeftPoint = regressionPoints[0];
-        var regRightPoint = regressionPoints[1];
-        var e1RM = AnalysisSelectors.getE1RM(state);
-        maxX = Math.max(maxX, regRightPoint.x);
-    } else {
-        // no regression line
-        var regLeftPoint = null;
-        var regRightPoint = null;
-        var e1RM = null;
-    }
-
     return {
         velocity: AnalysisSelectors.getAnalysisVelocity(state),
-        e1RM: e1RM,    
+        e1RM: getE1RM(state),    
         metric: SettingsSelectors.getDefaultMetric(state),
         r2: AnalysisSelectors.getR2(state),
-        shouldDisplayRegression: shouldDisplayRegression,
-        activeChartData: activeChartData,
-        errorChartData: errorChartData,
-        unusedChartData: unusedChartData,
-        regLeftPoint: regLeftPoint,
-        regRightPoint: regRightPoint,
-        minX: minX,
-        maxX: maxX,
-        maxY: maxY,
+        shouldDisplayRegression: getShouldDisplayRegression(state),
+        activeChartData: AnalysisSelectors.getActiveChartData(state),
+        errorChartData: AnalysisSelectors.getErrorChartData(state),
+        unusedChartData: AnalysisSelectors.getUnusedChartData(state),
+        regLeftPoint: getRegLeftPoint(state),
+        regRightPoint: getRegRightPoint(state),
+        minX: getMinX(state),
+        maxX: getMaxX(state),
+        maxY: getMaxY(state),
         dragged: AnalysisSelectors.getAnalysisDragged(state),
     };
 };
