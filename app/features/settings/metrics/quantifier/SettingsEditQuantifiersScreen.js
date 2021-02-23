97daeefdbf44b0cf8941c64ffabac820bb699d94
JonnyLinja <jonny.linja@gmail.com>
2021-02-22 22:16:07 -0500

    tweaked settings edit quantifier screen to do less checks

--- app/features/settings/metrics/quantifier/SettingsEditQuantifiersScreen.js
+++ app/features/settings/metrics/quantifier/SettingsEditQuantifiersScreen.js
@@ -3,13 +3,6 @@ import { connect } from 'react-redux';
 import { Platform } from 'react-native';
 
 import {
-    EMPTY_METRIC,
-    AVG_VELOCITY_METRIC,
-    RPE_METRIC,
-    DURATION_METRIC,
-    ROM_METRIC,
-    PKH_METRIC,
-    PKV_METRIC,
     EMPTY_QUANTIFIER,
     FIRST_REP_QUANTIFIER,
     LAST_REP_QUANTIFIER,
@@ -33,60 +26,66 @@ const pickerItem = (quantifier) => ({
     value: quantifier,
 });
 
-const mapStateToProps = (state, ownProps) => {
-    const items = [
-        pickerItem(EMPTY_QUANTIFIER),
-        pickerItem(AVG_QUANTIFIER), // vel, pkv, dur
-        pickerItem(FASTEST_EVER_QUANTIFIER), // vel, pkv, dur
-        pickerItem(SLOWEST_EVER_QUANTIFIER), // vel, pkv, dur
-        pickerItem(ABS_LOSS_QUANTIFIER), // vel, pkv, dur, rom
-        pickerItem(PERCENT_LOSS_QUANTIFIER), // vel, pkv, dur, rom
-        pickerItem(SET_LOSS_QUANTIFIER), // vel, pkv, dur, rom
-        pickerItem(PEAK_END_QUANTIFIER), // vel, pkv, dur, rom
-        pickerItem(FIRST_REP_QUANTIFIER), // all
-        pickerItem(LAST_REP_QUANTIFIER), // all
-        pickerItem(MIN_QUANTIFIER), // all
-        pickerItem(MAX_QUANTIFIER), // all
-    ];
+const items = [
+    pickerItem(EMPTY_QUANTIFIER),
+    pickerItem(AVG_QUANTIFIER), // vel, pkv, dur
+    pickerItem(FASTEST_EVER_QUANTIFIER), // vel, pkv, dur
+    pickerItem(SLOWEST_EVER_QUANTIFIER), // vel, pkv, dur
+    pickerItem(ABS_LOSS_QUANTIFIER), // vel, pkv, dur, rom
+    pickerItem(PERCENT_LOSS_QUANTIFIER), // vel, pkv, dur, rom
+    pickerItem(SET_LOSS_QUANTIFIER), // vel, pkv, dur, rom
+    pickerItem(PEAK_END_QUANTIFIER), // vel, pkv, dur, rom
+    pickerItem(FIRST_REP_QUANTIFIER), // all
+    pickerItem(LAST_REP_QUANTIFIER), // all
+    pickerItem(MIN_QUANTIFIER), // all
+    pickerItem(MAX_QUANTIFIER), // all
+];
 
-    if (Platform.OS === 'ios') {
-        return {
-            isModalShowing: CollapsedSettingsSelectors.getIsEditingQuantifier(state),
-            items: items,
-            selectedValue: CollapsedSettingsSelectors.getCurrentQuantifier(state),
-        };
-    } else {
-        switch(ownProps.rank) {
-            case 1:
-                return {
-                    items: items,
-                    selectedValue: CollapsedSettingsSelectors.getQuantifier1(state),
-                };
-            case 2:
-                return {
-                    items: items,
-                    selectedValue: CollapsedSettingsSelectors.getQuantifier2(state),
-                };
-            case 3:
-                return {
-                    items: items,
-                    selectedValue: CollapsedSettingsSelectors.getQuantifier3(state),
-                };
-            case 4:
-                return {
-                    items: items,
-                    selectedValue: CollapsedSettingsSelectors.getQuantifier4(state),
-                };
-            case 5:
-                return {
-                    items: items,
-                    selectedValue: CollapsedSettingsSelectors.getQuantifier5(state),
-                };
-            default:
-                return {};
-        }
+const mapStateToPropsiOS = (state) => {
+    return {
+        isModalShowing: CollapsedSettingsSelectors.getIsEditingQuantifier(state),
+        items,
+        selectedValue: CollapsedSettingsSelectors.getCurrentQuantifier(state),
+    };
+};
+
+// not sure if memoization is worth it
+// memoizing would require ref checks of 6 items
+// not memoizing just checks rank up to 6 times, so it can end FASTER, though it does need to generate the final return value
+const mapStateToPropsAndroid = (state, ownProps) => {
+    switch(ownProps.rank) {
+        case 1:
+            return {
+                items,
+                selectedValue: CollapsedSettingsSelectors.getQuantifier1(state),
+            };
+        case 2:
+            return {
+                items,
+                selectedValue: CollapsedSettingsSelectors.getQuantifier2(state),
+            };
+        case 3:
+            return {
+                items,
+                selectedValue: CollapsedSettingsSelectors.getQuantifier3(state),
+            };
+        case 4:
+            return {
+                items,
+                selectedValue: CollapsedSettingsSelectors.getQuantifier4(state),
+            };
+        case 5:
+            return {
+                items,
+                selectedValue: CollapsedSettingsSelectors.getQuantifier5(state),
+            };
+        default:
+            return {};
     }
 };
+
+// this way only check OS once
+const mapStateToProps = Platform.OS === 'ios' ? mapStateToPropsiOS : mapStateToPropsAndroid;
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     if (Platform.OS === 'ios') {
