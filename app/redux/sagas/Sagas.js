a0be467ca6fec9d73954600c139033d51da27698
JonnyLinja <jonny.linja@gmail.com>
2022-03-24 21:57:08 -0400

    International mode, removing google sign in and firebase analytics, plus enabling history and analysis to function without the cloud

--- app/redux/sagas/Sagas.js
+++ app/redux/sagas/Sagas.js
@@ -1,19 +1,19 @@
 import { all } from 'redux-saga/effects';
 
-import KillSwitchSaga from './KillSwitchSaga';
-import InitConfigSaga from './InitConfigSaga';
-import TokenSaga from './TokenSaga';
-import AuthSaga from './AuthSaga';
+// import KillSwitchSaga from './KillSwitchSaga';
+// import InitConfigSaga from './InitConfigSaga';
+// import TokenSaga from './TokenSaga';
+// import AuthSaga from './AuthSaga';
 import BluetoothSaga from './BluetoothSaga';
 import SuggestionsSaga from './SuggestionsSaga';
-import SyncSaga from './SyncSaga';
+// import SyncSaga from './SyncSaga';
 import TimerSaga from './TimerSaga';
 import TimerUnlockSaga from './TimerUnlockSaga';
 import EndOldWorkoutSaga from './EndOldWorkoutSaga';
 import ReconnectSaga from './ReconnectSaga';
-import SurveySaga from './SurveySaga';
-import InitializedAnalyticsSaga from './InitializedAnalyticsSaga';
-import OneRMAnalyticsSaga from './OneRMAnalyticsSaga';
+// import SurveySaga from './SurveySaga';
+// import InitializedAnalyticsSaga from './InitializedAnalyticsSaga';
+// import OneRMAnalyticsSaga from './OneRMAnalyticsSaga';
 // import VelocityThresholdSaga from './VelocityThresholdSaga';
 import OTASaga from './OTASaga';
 import BulkDataSaga from './BulkDataSaga';
@@ -23,20 +23,20 @@ import WorkingSetDeviceSaga from './WorkingSetDeviceSaga';
 
 const Sagas = function* Sagas(dispatch) {
     yield all([
-        KillSwitchSaga(),
-        InitConfigSaga(),
-        TokenSaga(),
-        AuthSaga(),
+        // KillSwitchSaga(),
+        // InitConfigSaga(),
+        // TokenSaga(),
+        // AuthSaga(),
         BluetoothSaga(),
         SuggestionsSaga(),
-        SyncSaga(),
+        // SyncSaga(),
         TimerSaga(),
         TimerUnlockSaga(),
         EndOldWorkoutSaga(),
         ReconnectSaga(),
-        SurveySaga(),
-        InitializedAnalyticsSaga(),
-        OneRMAnalyticsSaga(),
+        // SurveySaga(),
+        // InitializedAnalyticsSaga(),
+        // OneRMAnalyticsSaga(),
         OTASaga(dispatch),
         BulkDataSaga(),
         CalibrationSaga(),
