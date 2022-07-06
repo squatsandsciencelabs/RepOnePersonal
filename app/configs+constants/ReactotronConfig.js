414ca3858dd1b462974eb6539a38e94b0c84b3c9
JonnyLinja <jonny.linja@gmail.com>
2022-07-06 13:48:45 -0400

    added settings bundle. Reactotron should now run for production

--- app/configs+constants/ReactotronConfig.js
+++ app/configs+constants/ReactotronConfig.js
@@ -1,3 +1,4 @@
+import { Settings } from "react-native";
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
@@ -5,13 +6,13 @@ import sagaPlugin from 'reactotron-redux-saga';
 if (__DEV__) {
     // development, enable reactotron
     Reactotron
-    .configure({
-        name: 'RepOne',
-        host: '192.168.1.102',
-    }) // controls connection & communication settings
-    .use(sagaPlugin()) // add all built-in react native plugins
-    .use(reactotronRedux()) // add all built-in react native plugins
-    .connect() // let's connect!
+        .configure({
+            name: 'RepOne',
+            host: '192.168.1.102',
+        }) // controls connection & communication settings
+        .use(sagaPlugin()) // add all built-in react native plugins
+        .use(reactotronRedux()) // add all built-in react native plugins
+        .connect() // let's connect!
 
     console.tron = {
         log: (msg) => {
@@ -23,9 +24,35 @@ if (__DEV__) {
             Reactotron.display(obj);
         },
     };
+
+    const host = Settings.get('reactotron_ip');
+    console.tron.log(`host is ${host}`);
 } else {
-    // production, remove tron logs
-    console.tron = {};
-    console.tron.log = () => {};
-    console.tron.display = () => {};
+    const host = Settings.get('reactotron_ip');
+    if (!host) {
+        console.tron = {};
+        console.tron.log = console.log;
+        console.tron.display = () => { };
+    } else {
+
+        Reactotron
+            .configure({
+                name: 'RepOne',
+                host,
+            }) // controls connection & communication settings
+            .use(sagaPlugin()) // add all built-in react native plugins
+            .use(reactotronRedux()) // add all built-in react native plugins
+            .connect() // let's connect!
+
+        console.tron = {
+            log: (msg) => {
+                console.log(msg);
+                Reactotron.log(msg);
+            },
+            display: (obj) => {
+                console.log(obj.value)
+                Reactotron.display(obj);
+            },
+        };
+    }
 }
