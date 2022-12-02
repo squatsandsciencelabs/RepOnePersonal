c1fe29f89d46e90362b1cfc3dde9075f069fdda1
Nikita Oleinykov <nikita.o@develops.today>
2022-12-02 11:47:49 +0200

    chore: check for kratosEnabled for connection icons

--- app/features/settings/device/panels/SettingsDevicePanelConnected.js
+++ app/features/settings/device/panels/SettingsDevicePanelConnected.js
@@ -6,15 +6,17 @@ import {
 } from 'react-native';
 
 import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
+import { kratosEnabled } from 'app/configs+constants/KratosConfig';
 
 // displays connected device info, allows disconnect from device
 class SettingsDevicePanelConnected extends Component {
 
     _renderConnectedIcon() {
         const device = this.props.device;
-        var img = device.startsWith('Kratos')
-            ? require('app/appearance/images/icon_bluetooth_kratos_connected.png')
-            : require('app/appearance/images/icon_bluetooth_connected.png');
+        var img =
+            kratosEnabled && device.startsWith('Kratos')
+                ? require('app/appearance/images/icon_bluetooth_kratos_connected.png')
+                : require('app/appearance/images/icon_bluetooth_connected.png');
 
         return <Image source={img} />;
     }
