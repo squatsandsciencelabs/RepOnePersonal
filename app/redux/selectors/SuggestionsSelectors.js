4e978bc609d5a84418b4c9ea6994c0fc68b1021a
Nikita <nikita.develops.work@gmail.com>
2022-12-27 19:18:39 +0200

    feat: kratos modal, suggestions, EditWorkoutKratosDiscsActions

--- app/redux/selectors/SuggestionsSelectors.js
+++ app/redux/selectors/SuggestionsSelectors.js
@@ -23,12 +23,12 @@ export const generateKratosDiscsSuggestions = (state, input, ignore = []) => {
         kratosDiscsModel(state),
         input,
         null,
-        true,
+        false,
         ignore,
     );
 };
 
-const generateSuggestions = (model, input, bias, showBug=false, ignore = []) => {
+const generateSuggestions = (model, input, bias, showBug = false, ignore = []) => {
     // lowercase comparisons
     var lowercaseInput = '';
     if (input !== null) {
@@ -66,26 +66,29 @@ const generateSuggestions = (model, input, bias, showBug=false, ignore = []) => 
     }
 
     // just the suggestions
-    matches = matches.map((match) => match.suggestion);
+    matches = matches.map(match => {
+        const { seed, ...rawSuggestion } = match;
+        return rawSuggestion;
+    });
 
     // hack - ensure bug is last
     // TODO: if we do more than just the special bug tag, make this a generic system instead so you can add more types
     if (showBug) {
         // remove all bugs from suggestions so you can re-add it at the end
-        matches = matches.filter((e) => e !== 'bug');
+        matches = matches.filter((e) => e.suggestion !== 'bug');
 
         // add Bug if possible
-        if (ignore.filter((e) => e === 'bug').length <= 0) {
+        if (ignore.filter((e) => e.suggestion === 'bug').length <= 0) {
             if (matches.length === 10) {
-                matches[9] = 'bug';
+                matches[9].suggestion = 'bug';
             } else {
-                matches[matches.length] = 'bug';
+                matches[matches.length] = { suggestion: 'bug' };
             }
         }
     }
 
     // remove if it's exactly equal
-    if (matches.length > 0 && matches[0] === input) {
+    if (matches.length > 0 && matches[0].suggestion === input) {
         matches.shift();
     }
 
