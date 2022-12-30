f6e1225aa5faad046aeb5f0c35eb92bb14dd1e2c
Nikita <nikita.develops.work@gmail.com>
2022-12-30 18:40:01 +0200

    feat: kratos auto-remove reps configuration

--- app/redux/reducers/SetsReducer.js
+++ app/redux/reducers/SetsReducer.js
@@ -688,6 +688,7 @@ const addKratosRepData = (state, action) => {
         ePeakAngularAcceleration: action.ePeakAngularAcceleration,
         ePeakPower: action.ePeakPower,
         deviceFamily,
+        deleted: set.reps.length < action.kratosAutoDeleteReps,
     };
 
     let setChanges = {
--- app/redux/reducers/SettingsReducer.js
+++ app/redux/reducers/SettingsReducer.js
@@ -9,37 +9,43 @@ import {
     DISMISS_DEFAULT_METRIC,
     UPDATE_SYNC_DATE,
     EXPORTING_CSV,
+    SAVE_KRATOS_AUTO_DELETE_REPS,
+    PRESENT_KRATOS_AUTO_DELETE_REPS,
+    DISMISS_KRATOS_AUTO_DELETE_REPS,
 } from 'app/configs+constants/ActionTypes';
+import { DEFAULT_KRATOS_AUTO_DELETE_REPS } from 'app/configs+constants/KratosConfig';
 
 const defaultState = {
     defaultMetric: 'kgs',
     isEditingDefaultMetric: false,
     endSetTimerDuration: 30,
     isEditingEndSetTimer: false,
+    kratosAutoDeleteReps: DEFAULT_KRATOS_AUTO_DELETE_REPS,
+    isEditingKratosAutoDeleteReps: false,
     syncDate: '',
     wasTimerEdited: false,
     wasMetricEdited: false,
     isExportingCSV: false,
     lastExportCSVDate: null,
-    velocityThreshold: .1,
+    velocityThreshold: 0.1,
     isShowingVisualization: false,
 };
 
 const SettingsReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case SAVE_DEFAULT_METRIC: 
+        case SAVE_DEFAULT_METRIC:
             return Object.assign({}, state, {
                 defaultMetric: action.defaultMetric,
             });
         case PRESENT_DEFAULT_METRIC:
             return Object.assign({}, state, {
                 isEditingDefaultMetric: true,
-            }); 
+            });
         case DISMISS_DEFAULT_METRIC:
             return Object.assign({}, state, {
                 isEditingDefaultMetric: false,
             });
-        case SAVE_END_SET_TIMER:           
+        case SAVE_END_SET_TIMER:
             return Object.assign({}, state, {
                 endSetTimerDuration: action.endSetTimerDuration,
                 wasTimerEdited: true,
@@ -52,16 +58,28 @@ const SettingsReducer = (state = defaultState, action) => {
             return Object.assign({}, state, {
                 isEditingEndSetTimer: false,
             });
+        case SAVE_KRATOS_AUTO_DELETE_REPS:
+            return Object.assign({}, state, {
+                kratosAutoDeleteReps: action.autoDeleteReps,
+            });
+        case PRESENT_KRATOS_AUTO_DELETE_REPS:
+            return Object.assign({}, state, {
+                isEditingKratosAutoDeleteReps: true,
+            });
+        case DISMISS_KRATOS_AUTO_DELETE_REPS:
+            return Object.assign({}, state, {
+                isEditingKratosAutoDeleteReps: false,
+            });
         case UPDATE_SET_DATA_FROM_SERVER:
         case LOGIN_SUCCESS:
         case UPDATE_SYNC_DATE:
             return Object.assign({}, state, {
-                syncDate: action.syncDate
+                syncDate: action.syncDate,
             });
         case EXPORTING_CSV:
             return Object.assign({}, state, {
                 isExportingCSV: action.isExportingCSV,
-                lastExportCSVDate: new Date()
+                lastExportCSVDate: new Date(),
             });
         default:
             return state;
