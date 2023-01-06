ad76489f022da0b806cd71a52cec96eeb4cc76c1
Nikita <nikita.develops.work@gmail.com>
2023-01-06 15:21:23 +0200

    feat: added expo-media-library for VideRecorder.js

--- app/shared_features/camera/VideoRecorder.js
+++ app/shared_features/camera/VideoRecorder.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 import { CameraRoll } from '@react-native-camera-roll/camera-roll';
-import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
 import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake';
 import * as Device from 'app/utility/Device';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -23,7 +23,7 @@ function record(props, camera) {
                 const uri = await CameraRoll.save(video.path);
 
                 // delete from cache
-                await FileSystem.deleteAssetsAsync(video.path);
+                await MediaLibrary.deleteAssetsAsync(video.path);
 
                 // dispatch info
                 if (props.setID) {
