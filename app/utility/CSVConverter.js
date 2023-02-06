5337183ba02db7b3839b811de412dd9507e6ad2a
Nikita <nikita.develops.work@gmail.com>
2023-02-06 12:32:20 +0200

    feat: changed metrics order in csv, additional kratos metrics

--- app/utility/CSVConverter.js
+++ app/utility/CSVConverter.js
@@ -4,6 +4,7 @@
 import * as DateUtils from 'app/utility/DateUtils';
 import * as SetUtils from 'app/utility/SetUtils';
 import { getKratosEnabled } from 'app/configs+constants/KratosConfig';
+import { getKratosRepRows } from 'app/utility/SetUtils';
 
 // pass this the history sets as a sorted array
 // aka SetReducer's getHistorySets convenience function
@@ -13,7 +14,10 @@ export const convert = data => {
     const isKratosEnabled = getKratosEnabled();
 
     let output = isKratosEnabled
-        ? 'Exercise,Set,Rep,Weight,Metric,Set RPE,Tags,Workout Start Time,Rest Time,Concentric Avg Velocity (m/s),Eccentric Avg Velocity (m/s),Concentric Range of Motion (mm),Eccentric Range of Motion (mm),Concentric Peak Velocity (m/s),Eccentric Peak Velocity (m/s),Concentric Peak Velocity Location (%),Eccentric Peak Velocity Location (%),Concentric Duration of rep (sec),Eccentric Duration of rep (sec)\n'
+        ? 'Exercise,Set,Rep,Weight,Metric,Set RPE,Tags,Workout Start Time,Rest Time,Concentric Avg Velocity (m/s),Concentric Range of Motion (mm),Concentric Peak Velocity (m/s),Concentric Peak Velocity Location (%),Concentric Duration of rep (sec),' +
+          'Concentric Force,Concentric Force Loc,Concentric Power,Concentric Power Loc,Concentric Work,Concentric Work Loc,' +
+          'Eccentric Avg Velocity (m/s),Eccentric Range of Motion (mm),Eccentric Peak Velocity (m/s),Eccentric Peak Velocity Location (%),Eccentric Duration of rep (sec),' +
+          'Eccentric Force,Eccentric Force Loc,Eccentric Power,Eccentric Power Loc,Eccentric Work,Eccentric Work Loc,\n'
         : 'Exercise,Set,Rep,Weight,Metric,Set RPE,Tags,Workout Start Time,Rest Time,Avg Velocity (m/s),Range of Motion (mm),Peak Velocity (m/s),Peak Velocity Location (%),Duration of rep (sec)\n';
 
     // filter removed sets
@@ -71,44 +75,44 @@ export const convert = data => {
         }
 
         if (isKratosEnabled) {
-            if (set.deviceType === 'Kratos') {
-                reps.forEach((rep, index) => {
-                    output += getCommonData(
-                        set,
-                        index,
-                        setCount,
-                        tags,
-                        workoutStartTime,
-                        rest,
-                    );
+            reps.forEach((rep, index) => {
+                output += getCommonData(
+                    set,
+                    index,
+                    setCount,
+                    tags,
+                    workoutStartTime,
+                    rest,
+                );
+
+                if (set.deviceType === 'Kratos') {
+                    // concentric metrics
                     output += (rep.cAvgLinearVelocity / 1000).toFixed(2) + ',';
-                    output += (rep.eAvgLinearVelocity / 1000).toFixed(2) + ',';
                     output += rep.cRom + ',';
-                    output += rep.eRom + ',';
                     output += (rep.cPeakLinearVelocity / 1000).toFixed(2) + ',';
-                    output += (rep.ePeakLinearVelocity / 1000).toFixed(2) + ',';
                     output += rep.cPeakVelocityLocation + ',';
-                    output += rep.ePeakVelocityLocation + ',';
                     output += rep.cDuration / 1000 + ',';
-                    output += rep.eDuration / 1000 + '\n';
-                });
-            } else {
-                reps.forEach((rep, index) => {
-                    output += getCommonData(
-                        set,
-                        index,
-                        setCount,
-                        tags,
-                        workoutStartTime,
-                        rest,
-                    );
-                    output += (rep.averageVelocity / 1000).toFixed(2) + ',,';
-                    output += rep.rom + ',,';
-                    output += (rep.peakVelocity / 1000).toFixed(2) + ',,';
-                    output += rep.peakHeight + ',,';
-                    output += rep.duration / 1000 + ',' + '\n';
-                });
-            }
+                    // TODO: implement concentric force, force loc, power, power loc, work, work loc
+                    output += skipColumns(6);
+                    // eccentric metrics
+                    output += (rep.eAvgLinearVelocity / 1000).toFixed(2) + ',';
+                    output += rep.eRom + ',';
+                    output += (rep.ePeakLinearVelocity / 1000).toFixed(2) + ',';
+                    output += rep.ePeakVelocityLocation + ',';
+                    output += rep.eDuration / 1000 + ',';
+                    // TODO: implement eccentric force, force loc, power, power loc, work, work loc
+                    output += skipColumns(6) + '\n';
+                } else {
+                    // concentric metrics
+                    output += (rep.averageVelocity / 1000).toFixed(2) + ',';
+                    output += rep.rom + ',';
+                    output += (rep.peakVelocity / 1000).toFixed(2) + ',';
+                    output += rep.peakHeight + ',';
+                    output += rep.duration / 1000 + ',';
+                    // skip eccentric metrics
+                    output += skipColumns(17) + '\n';
+                }
+            });
         } else {
             reps.forEach((rep, index) => {
                 output += getCommonData(
@@ -153,4 +157,8 @@ const escapeDoubleQuote = value => {
     } else {
         return value;
     }
+};
+
+const skipColumns = n => {
+    return ','.repeat(n);
 };
