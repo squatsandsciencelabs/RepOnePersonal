1eb80362a946d024c8caf858602526034295650b
Nikita <nikita.develops.work@gmail.com>
2023-02-15 17:51:13 +0200

    feat: changes to set analysis UI, quantifier/metrics abbreviations

--- app/shared_features/set_card/analysis/SetAnalysis.js
+++ app/shared_features/set_card/analysis/SetAnalysis.js
@@ -1,66 +1,99 @@
-import React, {Component} from 'react';
-import {
-    View,
-    StyleSheet,
-    Text,
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import * as Device from 'app/utility/Device';
 
 class SetAnalysis extends Component {
-
-    _renderAnalysis(value, unit, description, isBigMetric) {
+    _renderAnalysis(value, unit, description, isBigMetric, key) {
         if (isBigMetric) {
             return (
-                <View style={styles.bigMetricBackground}>
+                <View
+                    style={styles.bigMetricBackground}
+                    key={`analysis-${key}`}>
                     <Text style={styles.bigMetricText}>{value}</Text>
                     <Text style={styles.bigMetric}>{unit}</Text>
-                    <Text style={styles.bigMetric}>{description}</Text>
+                    <Text style={styles.bigMetric} numberOfLines={2}>
+                        {description}
+                    </Text>
                 </View>
             );
         } else {
-            if (unit !== null && unit !== '') {
-                var text = unit + "\n" + description;
-            } else {
-                var text = description;
-            }
-
-            let textStyle = !this.props.rpe && text.includes("RPE") ? styles.redText : styles.text;
-            let metricStyle = !this.props.rpe && text.includes("RPE") ? styles.redMetric : styles.metric;
+            let textStyle =
+                !this.props.rpe && description.includes('RPE')
+                    ? styles.redText
+                    : styles.text;
+            let metricStyle =
+                !this.props.rpe && description.includes('RPE')
+                    ? styles.redMetric
+                    : styles.metric;
 
             return (
-                <View style={{flex: 1, marginTop: -2, justifyContent: 'center'}}>
+                <View style={styles.metricWrapper} key={`analysis-${key}`}>
                     <Text style={textStyle} numberOfLines={1}>
                         {value}
                     </Text>
-                    <Text style={metricStyle}>{text}</Text>
-                </View>  
+                    <Text style={metricStyle}>{unit}</Text>
+                    <Text style={metricStyle} numberOfLines={2}>
+                        {description}
+                    </Text>
+                </View>
             );
         }
+    }
+
+    _renderAnalysisItems() {
+        const analysis = [];
+
+        for (let i = 1; i <= 4; i++) {
+            const value = this.props[`value${i}`];
+            const unit = this.props[`unit${i}`];
+            const description = this.props[`description${i}`];
+
+            const isBigMetric = i === 1;
+
+            analysis.push(
+                this._renderAnalysis(value, unit, description, isBigMetric, i),
+            );
+        }
+
+        return analysis;
     }
 
     render() {
         let lastColumn = null;
         if (!Device.isSmallDevice()) {
-            lastColumn = this._renderAnalysis(this.props.value5, this.props.unit5, this.props.description5, false);
+            lastColumn = [
+                this._renderAnalysis(
+                    this.props.value5,
+                    this.props.unit5,
+                    this.props.description5,
+                    false,
+                    5,
+                ),
+            ];
         }
 
         return (
-            <View style={[styles.border, styles.container, {flex:1, flexDirection: 'row'}]}>
-                {this._renderAnalysis(this.props.value1, this.props.unit1, this.props.description1, true)}
-                {this._renderAnalysis(this.props.value2, this.props.unit2, this.props.description2, false)}
-                {this._renderAnalysis(this.props.value3, this.props.unit3, this.props.description3, false)}
-                {this._renderAnalysis(this.props.value4, this.props.unit4, this.props.description4, false)}
-                {lastColumn}
+            <View
+                style={[
+                    styles.border,
+                    styles.container,
+                    { flex: 1, flexDirection: 'row' },
+                ]}>
+                <ScrollView
+                    horizontal={true}
+                    contentContainerStyle={{ justifyContent: 'space-between' }}>
+                    {this._renderAnalysisItems()}
+                    {lastColumn}
+                </ScrollView>
             </View>
         );
     }
-
 }
 
 const styles = StyleSheet.create({
     container: {
         overflow: 'hidden',
-        backgroundColor:'white',
+        backgroundColor: 'white',
         paddingTop: 10,
         paddingBottom: 0,
         paddingLeft: 5,
@@ -83,36 +116,28 @@ const styles = StyleSheet.create({
     },
     text: {
         color: '#4d4d4d',
-        fontSize: 15,
-        fontWeight: 'bold',
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 5,
+        fontSize: 18,
+        fontWeight: '500',
+        marginTop: 8,
         textAlign: 'center',
-        paddingTop: 5,
-        paddingBottom: 5,
     },
     metric: {
         color: '#4d4d4d',
-        fontSize: 8,
-        fontWeight: "500",
-        paddingTop: 3,
-        paddingBottom: 5,
-        marginTop: -8,
-        marginLeft: 1,
-        marginRight: 1,
+        fontSize: 10,
+        fontWeight: '500',
         textAlign: 'center',
+        alignSelf: 'center',
     },
     redMetric: {
         color: 'red',
         fontSize: 8,
-        fontWeight: "500",
+        fontWeight: '500',
         paddingTop: 3,
         paddingBottom: 5,
         marginTop: -8,
         marginLeft: 3,
         marginRight: 3,
-        textAlign: 'center',        
+        textAlign: 'center',
     },
     bigMetricBackground: {
         alignItems: 'center',
@@ -123,20 +148,29 @@ const styles = StyleSheet.create({
         height: 80,
         borderRadius: 80 / 2,
         borderColor: '#fddddd',
-        overflow: 'hidden',        
+        overflow: 'hidden',
     },
     bigMetricText: {
         textAlign: 'center',
-        color: '#f0565a',        
-        fontSize: 20,
-        fontWeight: 'bold',
-    }, 
+        color: '#f0565a',
+        fontSize: 18,
+        fontWeight: '800',
+    },
     bigMetric: {
         textAlign: 'center',
         color: '#f0565a',
         fontSize: 10,
-        fontWeight: 'bold',        
-    }
+        fontWeight: '500',
+        paddingHorizontal: 16,
+        alignSelf: 'center',
+    },
+    metricWrapper: {
+        flex: 1,
+        paddingTop: 3,
+        alignItems: 'center',
+        width: 62,
+        marginLeft: 7,
+    },
 });
 
 export default SetAnalysis;
