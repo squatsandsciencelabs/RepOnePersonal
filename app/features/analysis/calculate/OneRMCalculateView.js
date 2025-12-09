db3553bae956f24dab232c87309ae456913c66db
JonnyLinja <github.editor309@passmail.net>
2025-12-09 16:09:06 -0500

    fix: analysis slider android visuals

--- app/features/analysis/calculate/OneRMCalculateView.js
+++ app/features/analysis/calculate/OneRMCalculateView.js
@@ -217,7 +217,7 @@ class OneRMView extends Component {
             var thumbTintColor = '#ffffff';
         } else {
             var thumbTintColor = '#368fff';
-            var maxTintColor = '#ffffff';
+            var maxTintColor = 'rgba(239, 239, 239, 1)';
         }
 
         return (
