d52cf99ae1fd2d91785906d57e3145db9e39c022
JonnyLinja <github.editor309@passmail.net>
2026-04-30 20:17:42 -0400

    fixed picker modal jumping to prev options

--- app/shared_features/picker/PickerModal.js
+++ app/shared_features/picker/PickerModal.js
@@ -1,68 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Modal, TouchableOpacity, Platform } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
-class PickerModal extends Component {
-    // ACTIONS
+export default function PickerModal(props) {
+    const [selectedValue, setSelectedValue] = useState(props.selectedValue);
 
-    _onValueChange(value) {
-        this.props.selectValue(value);
+    useEffect(() => {
+        setSelectedValue(props.selectedValue);
+    }, [props.selectedValue]);
+
+    function onValueChange(value) {
+        setSelectedValue(value);
+        props.selectValue(value);
     }
 
-    _close() {
-        this.props.closeModal();
-    }
-
-    // RENDER
-
-    render() {
-        if (Platform.OS === 'ios') {
-            return (
-                <Modal
-                    animationType={'slide'}
-                    transparent={true}
-                    visible={this.props.isModalShowing}>
-                    <View
-                        style={{
-                            flex: 1,
-                            backgroundColor: 'rgba(0,0,0,0.6)',
-                            justifyContent: 'flex-end',
-                        }}>
-                        <TouchableOpacity
-                            style={{ flex: 1 }}
-                            onPress={() => this._close()}></TouchableOpacity>
-
-                        <Picker
-                            style={{ backgroundColor: 'white' }}
-                            itemStyle={{ color: 'black' }}
-                            selectedValue={this.props.selectedValue}
-                            onValueChange={value => this._onValueChange(value)}>
-                            {this._renderItems()}
-                        </Picker>
-                    </View>
-                </Modal>
-            );
-        } else {
-            const color = this.props.color || 'rgba(47, 128, 237, 1)';
-            const dropdownIconColor = this.props.dropdownIconColor || 'white';
-
-            return (
-                <Picker
-                    style={{
-                        color: color,
-                    }}
-                    dropdownIconColor={dropdownIconColor}
-                    selectedValue={this.props.selectedValue}
-                    onValueChange={value => this._onValueChange(value)}>
-                    {this._renderItems()}
-                </Picker>
-            );
-        }
-    }
-
-    _renderItems() {
+    function renderItems() {
         var count = 0;
-        return this.props.items.map(function (item) {
+        return props.items.map(function (item) {
             return (
                 <Picker.Item
                     key={count++}
@@ -72,6 +26,46 @@ class PickerModal extends Component {
             );
         });
     }
-}
 
-export default PickerModal;
+    if (Platform.OS === 'ios') {
+        return (
+            <Modal
+                animationType={'slide'}
+                transparent={true}
+                visible={props.isModalShowing}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'rgba(0,0,0,0.6)',
+                        justifyContent: 'flex-end',
+                    }}>
+                    <TouchableOpacity
+                        style={{ flex: 1 }}
+                        onPress={() => props.closeModal()}
+                    />
+
+                    <Picker
+                        style={{ backgroundColor: 'white' }}
+                        itemStyle={{ color: 'black' }}
+                        selectedValue={selectedValue}
+                        onValueChange={value => onValueChange(value)}>
+                        {renderItems()}
+                    </Picker>
+                </View>
+            </Modal>
+        );
+    } else {
+        const color = props.color || 'rgba(47, 128, 237, 1)';
+        const dropdownIconColor = props.dropdownIconColor || 'white';
+
+        return (
+            <Picker
+                style={{ color: color }}
+                dropdownIconColor={dropdownIconColor}
+                selectedValue={selectedValue}
+                onValueChange={value => onValueChange(value)}>
+                {renderItems()}
+            </Picker>
+        );
+    }
+}
