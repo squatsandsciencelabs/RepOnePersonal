629ebce7018333e60c1a05347a3e242fd805e23f
JonnyLinja <github.editor309@passmail.net>
2026-05-08 13:46:57 -0400

    unfix: rebroke mtu to not introduce new changes

--- app/services/Bluetooth.js
+++ app/services/Bluetooth.js
@@ -111,7 +111,7 @@ export default async function (store) {
 
                 // mtu
                 try {
-                    const mtu = await BleManager.requestMTU(
+                    const mtu = BleManager.requestMTU(
                         args.peripheral,
                         MTU_SIZE,
                     );
